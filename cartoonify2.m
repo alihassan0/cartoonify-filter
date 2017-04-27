e5fa4ef62de376545cf5641488f232a72e7ee0fd
Ali hassan <alithephantom@gmail.com>
2017-04-27 16:39:40 +0200

    clean useless code and add comments

--- cartoonify2.m
+++ cartoonify2.m
@@ -1,18 +1,18 @@
-img = imread('img.jpg');
+% read image
+img = imread('test2.jpg');
 
+% convert image to HSV
 hsv = rgb2hsv(img);
-% subplot(3,3,4), imhist(img(:,:,1)) , title('red channel')
-% subplot(3,3,5), imhist(img(:,:,2)), title('blue channel')
-% subplot(3,3,6), imhist(img(:,:,3)), title('green channel')
 
+% seperate HSV into it's three channels
 hueMat = hsv(:, :, 1);
 saturationMat = hsv(:, :, 2);
 valueMat = hsv(:, :, 3);
-hue = hsv(:, :, 1);
 
 quantize = @(pixel, quantizationStep ) (floor(pixel/quantizationStep)+.5)*quantizationStep;
 
-% modify hue mat
+% modify hue channel 
+
 mat = hueMat;
 [rows, cols] = size(mat);
 for i=1:1:rows
@@ -25,7 +25,7 @@ hueMat = mat ;
 
 % modify saturation mat
 mat = saturationMat;
-[rows, cols] = size(mat)
+[rows, cols] = size(mat);
 for i=1:1:rows
     for j=1:1:cols
         
@@ -41,7 +41,7 @@ saturationMat= mat ;
 
 % modify value mat
 mat = valueMat;
-[rows, cols] = size(mat)
+[rows, cols] = size(mat);
 for i=1:1:rows
     for j=1:1:cols
         pixel = mat(i, j);
@@ -58,36 +58,42 @@ for i=1:1:rows
 end
 valueMat= mat ;
 
-    
-% saturation(:) = 1;
-% value(:) = 0;
+%merge hsv channels
 hsv = cat(3, hueMat, saturationMat, valueMat);
+
+%convert HSV image to RGB 
 rgb = hsv2rgb(hsv);
 
+%create a 3*3 structrung element;
 structuringElement =  strel('disk',1);
-rgb2 = imopen(rgb, structuringElement)
-rgb3 = imopen(img, structuringElement)
 
-BW = edge(img(:,:,1),'Sobel')|edge(img(:,:,2),'Sobel')|edge(img(:,:,3),'Sobel')
-BW = imdilate(BW,strel('line',2, 0))
+% open rgb image to clear rouge pixels
+rgb2 = imopen(rgb, structuringElement);
 
-subplot(2,3,1), imshow(img), title('originalImage');
-subplot(2,3,2), imshow(rgb), title('hsvtorgb');
-subplot(2,3,3), imshow(rgb2), title('opened Image');
-subplot(2,3,4), imshow(BW), title('3 channel edges');
-BW = double(~BW)
-BW = changem(BW,[.2],[0])
+% get original Image edges
+BW = edge(img(:,:,1),'Sobel')|edge(img(:,:,2),'Sobel')|edge(img(:,:,3),'Sobel');
+BW = imdilate(BW,strel('line',2, 0));
 
-imgwithEdges = cat(3, rgb2(:,:,1).*BW, rgb2(:,:,2).*BW, rgb2(:,:,3).*BW); 
-subplot(2,3,5), imshow(imgwithEdges), title('img with edges');
 
+% create mask from edges
+mask = double(~BW)
+% replace 
+mask = changem(BW,[.4],[0]);
+
+imgwithEdges = cat(3, rgb2(:,:,1).*mask, rgb2(:,:,2).*mask, rgb2(:,:,3).*mask); 
+% merge image mask with result from part 1
+
+% apply mean filter to colored image;
 h = fspecial('average', [3 3]);
 averagedImg1 = filter2(h, imgwithEdges(:,:,1));
 averagedImg2 = filter2(h, imgwithEdges(:,:,2));
 averagedImg3 = filter2(h, imgwithEdges(:,:,3));
 averagedImg = cat(3, averagedImg1, averagedImg2, averagedImg3);
 
+
+subplot(2,3,1), imshow(img), title('originalImage');
+subplot(2,3,2), imshow(rgb), title('hsvtorgb');
+subplot(2,3,3), imshow(rgb2), title('opened Image');
+subplot(2,3,4), imshow(BW), title('3 channel edges');
+subplot(2,3,5), imshow(imgwithEdges), title('img with edges');
 subplot(2,3,6), imshow(averagedImg), title('smoothed edges');
-
-
-% BW = edge(averagedImg1,'Sobel')+edge(averagedImg2,'Sobel')+edge(averagedImg3,'Sobel')
